ec13c7f628c3aa83ff9b1bffb3268dd41f997998
uditaswal <98157012+uditaswal@users.noreply.github.com>
2024-03-16 01:57:54 +0530

    Delete script4.js

--- script4.js
+++
@@ -1,9 +0,0 @@
-// let timerDisplay;
-
-// score = parseInt(localStorage.getItem("score")) || 0;
-// duration = parseInt(localStorage.getItem("duration")) || 0;
-// // Update the timer element
-// timerDisplay = document.getElementById("timer").innerText;
-// document.getElementById("timer").innerText = `${timerDisplay}`;
-
-// document.getElementById("scorebtn").textContent = `Score: ${++score}`;
